7a9b9a6dd1a86b423ab1368367e793ce0528f8e0
tothricsaj <tothricsaj@gmail.com>
2020-01-04 18:26:24 +0100

    working well

--- my-first-io.js
+++ my-first-io.js
@@ -1,4 +1,4 @@
 const fs = require('fs')
 const fileBuff = fs.readFileSync(process.argv[2]);
 
-console.log(String.prototype.split(fileBuff.toString(), '\n'))
+console.log(fileBuff.toString().split('\n').length)
