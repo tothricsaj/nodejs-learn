373dcf7625b3e4636a707bc012f44c563c2bbd96
tothricsaj <tothricsaj@gmail.com>
2020-01-04 18:28:46 +0100

    -1 :duck:

--- my-first-io.js
+++ my-first-io.js
@@ -1,4 +1,4 @@
 const fs = require('fs')
 const fileBuff = fs.readFileSync(process.argv[2]);
 
-console.log(fileBuff.toString().split('\n').length)
+console.log(fileBuff.toString().split('\n').length - 1)
